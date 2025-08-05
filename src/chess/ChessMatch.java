df005178423fadc7d8ab86856ee3cf6b5cdea7ea
sebastiao25759 <sebastiao.rodrigues.25759@aluno.facape.br>
2025-08-05 15:37:01 -0300

    Piece move count

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -82,7 +82,8 @@ public class ChessMatch {
     }
 
     private Piece makeMove(Position source, Position target) {
-        Piece p = board.removePiece(source);
+        ChessPiece p = (ChessPiece) board.removePiece(source);
+        p.increaseMoveCount();
         Piece capturedPiece = board.removePiece(target);
         if (capturedPiece != null) {
             piecesOnTheBoard.remove(capturedPiece);
@@ -93,9 +94,9 @@ public class ChessMatch {
     }
 
     private void undoMove(Position source, Position target, Piece capturedPiece) {
-        Piece p = board.removePiece(target);
+        ChessPiece p = (ChessPiece) board.removePiece(target);
         board.placePiece(p, source);
-
+        p.decreaseMove();
         if(capturedPiece != null) {
             board.placePiece(capturedPiece, target);
             capturedPieces.remove(capturedPiece);
